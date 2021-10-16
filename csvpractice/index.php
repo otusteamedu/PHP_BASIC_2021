2b186845bf73230547b8a85d6220cf2d8871771c
citikot <citikot@dr.com>
2021-10-16 21:29:32 +0300

    practice ready

--- csvpractice/index.php
+++ csvpractice/index.php
@@ -4,3 +4,36 @@
 1. Записать в файл нового клиента.
 2. Удалить первого клиента.
 */
+
+$clientsList = [];
+$clientInfo = [
+    "Name" => "",
+    "Surname" => "",
+    "Phone" => "",
+    "Email" => "",
+    "CreatedAt" => ""
+];
+
+$pointer = 0;
+$fs = fopen("clients.csv", "r+");
+while (($clientsList[$pointer] = fgetcsv($fs,0, ";")) !== false) {
+    $pointer += 1;
+}
+fclose($fs);
+
+array_pop($clientsList);
+array_splice($clientsList,1,1);
+
+foreach ($clientInfo as $key => $value) {
+    $prompt = "Please, enter " . $key . ": ";
+    $clientInfo[$key] = readline($prompt);
+}
+
+array_push($clientsList, $clientInfo);
+$fs = fopen("clients.csv", "w");
+foreach ($clientsList as $key => $value) {
+    $record = implode(";", $value) . PHP_EOL;
+    fwrite($fs, $record);
+}
+fclose($fs);
+
