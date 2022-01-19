0c513fd6e5aa673e64b5f38a6b0b21dc8eaebd46
VladislavCheremnov <88377818+VladislavCheremnov@users.noreply.github.com>
2022-01-19 17:17:26 +0700

    Выполнил все задания.

--- code.php
+++ code.php
@@ -1,5 +1,5 @@
 <?php
-    #Задание 1
+#Задание 1
 
         $num = 0;
             while ($num < 100) {
@@ -72,7 +72,64 @@
         echo replace('order filing out');
 
     #Задание 6
-
+    $nav = [
+        "Home" => ["1", "2", "3"],
+        "Service" => ["1", "2", "3"],
+        "Works" => ["1", "2", "3"],
+        "News" => ["1", "2", "3"],
+        "Contact" => ["1", "2", "3"],
+    ];
+    ?>
+    <!DOCTYPE html>
+    <html lang="ru">
+    
+    <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+      <link rel="stylesheet" href="css/style.css">
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+        crossorigin="anonymous"></script>
+      <title>Klean</title>
+    </head>
+    
+    <body>
+      <header>
+        <div class="navbar navbar-expand-lg">
+          <div class="container d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start header">
+    
+            <nav class="collapse navbar-collapse" id="navbarNav">
+              <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+    
+              <?
+              foreach ($nav as $key => $value) {
+                 ?>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="dropdown_1" data-bs-toggle="dropdown"
+                        aria-expanded="true"><?echo $key;?></a>
+                        <ul class="dropdown-menu" aria-labelledby="dropdown_1" data-bs-popper="none">
+                        <? foreach ($value as $val) {
+                            echo('<li><a class="dropdown-item" href="#">'. $val .'</a></li>');
+                            }
+                        ?>
+                        </ul>
+                    </li>
+                 <?
+              }
+              ?>
+              </ul>
+            </nav>
+        
+        </div>
+      </header>
+    </body>
+    
+    </html>
+    
+    <?php
     #Задание 7
 
         for ($i = 0 -1; $i < 9; print(++$i.PHP_EOL));
